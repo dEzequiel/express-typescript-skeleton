1eefb016daf56461c032fc805f8bc13234ad112f
eze <erosarosario@cifpfbmoll.eu>
2022-09-13 14:08:06 +0200

    add routes

--- src/routes/entity.ts
+++ src/routes/entity.ts
@@ -1,34 +1,26 @@
 import { Router, Response, Request } from "express";
+import { postEntity, getEntities, getEntity } from "../controllers/entity";
+import { getSpecificEntity } from "../services/entity";
 
 const router = Router();
 
 /**
  * /entity [GET]
  */
-router.get("/", (req: Request, res: Response) => {
-  res.send({ data: "ENTITY_MODEL" });
-});
+router.get("/", getEntities);
 
 /**
-* /entity/1 [GET]
-*/
-router.get("/:id", (req: Request, res: Response) => {
-    res.send('entity ' + req.params.id)
-})
+ * /entity/1 [GET]
+ */
+router.get("/:id", getEntity);
 
 /**
-* /entity [POST]
-*/
-router.post("/", (req: Request, res: Response) => {
+ * /entity [POST]
+ */
+router.post("/", postEntity);
 
-})
+router.put("/:id", (req: Request, res: Response) => {});
 
-router.put("/:id", (req: Request, res: Response) => {
-
-})
-
-router.delete("/:id", (req: Request, res: Response) => {
-
-})
+router.delete("/:id", (req: Request, res: Response) => {});
 
 export { router };
