89b78305e184d7bf281f79905f34a453cde330e4
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-09-16 19:36:10 +0200

    best commit ever

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,7 +1,8 @@
 import { Response } from "express";
+import { HttpCode } from "../exceptions/HttpCode";
 
 const handleHttp = (res: Response, error: string) => {
-  res.status(500);
+  res.status(HttpCode.INTERNAL_SERVER_ERROR;
   res.send({ error });
 };
 
